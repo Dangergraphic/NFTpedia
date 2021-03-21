8279630781ac31160072cab86e09342c22b247fb
Christofon <kremsnerchristopher@gmail.com>
2021-03-21 14:25:49 +0100

    start work on react frontend and erc721 contract

---
+++ frontend/src/components/Dapp.js
@@ -0,0 +1,220 @@
+import React from "react";
+
+
+import { ethers } from "ethers";
+
+import TokenArtifact from "../contracts/Token.json";
+import contractAddress from "../contracts/contract-address.json";
+
+
+import { NoWalletDetected } from "./NoWalletDetected";
+import { ConnectWallet } from "./ConnectWallet";
+import { TransactionErrorMessage } from "./TransactionErrorMessage";
+import { WaitingForTransactionMessage } from "./WaitingForTransactionMessage";
+
+const HARDHAT_NETWORK_ID = '31337';
+
+
+const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
+
+
+export class Dapp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+ 
+      tokenData: undefined,
+   
+      selectedAddress: undefined,
+      balance: undefined,
+      txBeingSent: undefined,
+      transactionError: undefined,
+      networkError: undefined,
+    };
+
+    this.state = this.initialState;
+  }
+
+  render() {
+    if (window.ethereum === undefined) {
+      return <NoWalletDetected />;
+    }
+
+    if (!this.state.selectedAddress) {
+      return (
+        <ConnectWallet 
+          connectWallet={() => this._connectWallet()} 
+          networkError={this.state.networkError}
+          dismiss={() => this._dismissNetworkError()}
+        />
+      );
+    }
+
+    return (
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-12">
+            <h1>
+              NFTpedia
+            </h1>
+            <p>
+              Welcome <b>{this.state.selectedAddress}</b>
+              .
+            </p>
+            <button onClick={() => this._mintTokens()}>Mint</button>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col-12">
+            {this.state.txBeingSent && (
+              <WaitingForTransactionMessage txHash={this.state.txBeingSent} />
+            )}
+
+            {this.state.transactionError && (
+              <TransactionErrorMessage
+                message={this._getRpcErrorMessage(this.state.transactionError)}
+                dismiss={() => this._dismissTransactionError()}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentWillUnmount() {
+    this._stopPollingData();
+  }
+
+  async _connectWallet() {
+    const [selectedAddress] = await window.ethereum.enable();
+
+    if (!this._checkNetwork()) {
+      return;
+    }
+
+    this._initialize(selectedAddress);
+
+    window.ethereum.on("accountsChanged", ([newAddress]) => {
+      this._stopPollingData();
+      if (newAddress === undefined) {
+        return this._resetState();
+      }
+      
+      this._initialize(newAddress);
+    });
+    
+    window.ethereum.on("networkChanged", ([networkId]) => {
+      this._stopPollingData();
+      this._resetState();
+    });
+  }
+
+  _initialize(userAddress) {
+
+    this.setState({
+      selectedAddress: userAddress,
+    });
+
+    this._intializeEthers();
+    this._getTokenData();
+    this._startPollingData();
+  }
+
+  async _intializeEthers() {
+    this._provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    this._token = new ethers.Contract(
+      contractAddress.Token,
+      TokenArtifact.abi,
+      this._provider.getSigner(0)
+    );
+  }
+
+  _startPollingData() {
+    this._pollDataInterval = setInterval(() => this._updateBalance(), 1000);
+
+    this._updateBalance();
+  }
+
+  _stopPollingData() {
+    clearInterval(this._pollDataInterval);
+    this._pollDataInterval = undefined;
+  }
+
+  async _getTokenData() {
+    const name = await this._token.name();
+    const symbol = await this._token.symbol();
+
+    this.setState({ tokenData: { name, symbol } });
+  }
+
+  async _updateBalance() {
+    const balance = await this._token.balanceOf(this.state.selectedAddress);
+    this.setState({ balance });
+  }
+
+  async _mintTokens() {
+
+    try {
+      this._dismissTransactionError();
+
+      // TODO map second parameter to input field
+      const tx = await this._token.awardItem("0x7396D21a2A9975814deE68217f1f1F13EBDBEE40", "works.com");
+      this.setState({ txBeingSent: tx.hash });
+
+      const receipt = await tx.wait();
+
+      if (receipt.status === 0) {
+        throw new Error("Transaction failed");
+      }
+
+      await this._updateBalance();
+    } catch (error) {
+      if (error.code === ERROR_CODE_TX_REJECTED_BY_USER) {
+        return;
+      }
+
+      console.error(error);
+      this.setState({ transactionError: error });
+    } finally {
+      this.setState({ txBeingSent: undefined });
+    }
+  }
+
+  _dismissTransactionError() {
+    this.setState({ transactionError: undefined });
+  }
+
+  _dismissNetworkError() {
+    this.setState({ networkError: undefined });
+  }
+
+  _getRpcErrorMessage(error) {
+    if (error.data) {
+      return error.data.message;
+    }
+
+    return error.message;
+  }
+
+  _resetState() {
+    this.setState(this.initialState);
+  }
+
+  _checkNetwork() {
+    if (window.ethereum.networkVersion === HARDHAT_NETWORK_ID) {
+      return true;
+    }
+
+    this.setState({ 
+      networkError: 'Please connect Metamask to Localhost:8545'
+    });
+
+    return false;
+  }
+}
---
+++ frontend/src/components/TokenList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function TokenList({ selectedAddress }) {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
